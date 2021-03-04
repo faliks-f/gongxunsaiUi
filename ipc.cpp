e718015f34acf6b7b34054c668c86bbf9aedeaff
faliks <243650719@qq.com>
2021-03-04 16:20:09 +0800

    second

---
+++ ipc.cpp
@@ -0,0 +1,10 @@
+﻿#include "ipc.h"
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+void startPython(std::string command)
+{
+    system(command.data());
+}
---
+++ ipc.h
@@ -0,0 +1,8 @@
+﻿#ifndef IPC_H
+#define IPC_H
+
+#include <string>
+
+void startPython(std::string);
+
+#endif // IPC_H
